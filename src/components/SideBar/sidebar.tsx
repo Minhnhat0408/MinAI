ef33fef02fb1315f89829ad3ea3b47504fded3a8
MinhMatMong <kurominhnhat@gmail.com>
2023-09-10 23:16:11 +0700

    fix eslint and remove unnecessary import for deployment

--- src/components/SideBar/sidebar.tsx
+++ src/components/SideBar/sidebar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import Image from "next/image";
 import { Montserrat } from 'next/font/google'
 import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
